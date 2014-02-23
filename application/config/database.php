807b39b62bcc5fd71debe8ff5b6b8a7649e4b32a
sithitat <sithitat@yahoo.com>
2014-02-23 14:31:02 +0700

    Test 20140223

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/*
+/*333
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
 | -------------------------------------------------------------------
